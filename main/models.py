31939b094ac8195800d227e8b87d6468883ec94b
aydotjs <ayobami66@gmail.com>
2024-10-28 14:34:58 +0100

    assignment model

--- main/models.py
+++ main/models.py
@@ -1,48 +1,58 @@
 from django.db import models
 from django.core import serializers
+
+
 # Teacher Model
 class Teacher(models.Model):
     full_name = models.CharField(max_length=100)
     # detail=models.TextField(null=True)
-    email= models.CharField(max_length=100)
-    password=models.CharField(max_length=100, blank=True, null=True)
-    qualification=models.CharField(max_length=200)
-    mobile_no=models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100, blank=True, null=True)
+    qualification = models.CharField(max_length=200)
+    mobile_no = models.CharField(max_length=20)
     profile_img = models.ImageField(upload_to="teacher_profile_imgs/", null=True)
-    skills=models.TextField()
-    
+    skills = models.TextField()
+
     class Meta:
-       verbose_name_plural="1. Teachers"
-       
+        verbose_name_plural = "1. Teachers"
+
     # Total Teacher Courses
     def total_teacher_courses(self):
         total_courses = Course.objects.filter(teacher=self).count()
         return total_courses
 
-# Total Teacher Chapters
+    # Total Teacher Chapters
     def total_teacher_chapters(self):
         total_chapters = Chapter.objects.filter(course__teacher=self).count()
         return total_chapters
 
-# Total Teacher Students
+    # Total Teacher Students
     def total_teacher_students(self):
-        total_students = StudentCourseEnrollment.objects.filter(course__teacher=self).count()
+        total_students = StudentCourseEnrollment.objects.filter(
+            course__teacher=self
+        ).count()
         return total_students
+
 
 # Course Category
 class CourseCategory(models.Model):
     title = models.CharField(max_length=100)
-    description= models.TextField()
-    
+    description = models.TextField()
+
     class Meta:
-       verbose_name_plural="2. Course Categories"
+        verbose_name_plural = "2. Course Categories"
+
     def __str__(self):
         return self.title
+
+
 # Course Model
 # Course Model
 class Course(models.Model):
     category = models.ForeignKey(CourseCategory, on_delete=models.CASCADE)
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name="teacher_courses")
+    teacher = models.ForeignKey(
+        Teacher, on_delete=models.CASCADE, related_name="teacher_courses"
+    )
     title = models.CharField(max_length=150)
     description = models.TextField()
     featured_img = models.ImageField(upload_to="course_imgs/", null=True)
@@ -57,53 +67,69 @@ class Course(models.Model):
         else:
             related_videos = Course.objects.none()  # or handle the logic in another way
 
-        return serializers.serialize('json', related_videos)
+        return serializers.serialize("json", related_videos)
+
     def __str__(self):
-         return self.title
-     
+        return self.title
+
     def total_enrolled_students(self):
-        total_enrolled_students = StudentCourseEnrollment.objects.filter(course=self).count()
+        total_enrolled_students = StudentCourseEnrollment.objects.filter(
+            course=self
+        ).count()
         return total_enrolled_students
-     
+
     def course_rating(self):
-        course_rating = CourseRating.objects.filter(course=self).aggregate(avg_rating=models.Avg("rating"))
+        course_rating = CourseRating.objects.filter(course=self).aggregate(
+            avg_rating=models.Avg("rating")
+        )
         return course_rating["avg_rating"]
+
 
 # Chapter Model
 class Chapter(models.Model):
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name = "course_chapters")
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, related_name="course_chapters"
+    )
     title = models.CharField(max_length=150)
     description = models.TextField()
-    video = models.FileField(upload_to='chapter_videos/', null=True)
+    video = models.FileField(upload_to="chapter_videos/", null=True)
     remarks = models.TextField(null=True)
 
     class Meta:
         verbose_name_plural = "4. Chapters"
 
-    
+
 # Student Model
 class Student(models.Model):
     full_name = models.CharField(max_length=100)
-    email= models.CharField(max_length=100)
-    password=models.CharField(max_length=100)
-    username=models.CharField(max_length=200)
-    interested_categories=models.TextField()
-    
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    username = models.CharField(max_length=200)
+    interested_categories = models.TextField()
+
     def __str__(self):
-         return self.full_name
+        return self.full_name
+
     class Meta:
-       verbose_name_plural="5. Students"
+        verbose_name_plural = "5. Students"
+
 
 # Student Course Enrollment
 class StudentCourseEnrollment(models.Model):
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='enrolled_courses')
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='enrolled_student')
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, related_name="enrolled_courses"
+    )
+    student = models.ForeignKey(
+        Student, on_delete=models.CASCADE, related_name="enrolled_student"
+    )
     enrolled_time = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name_plural = "6. Enrolled Courses"
+
     def __str__(self):
-         return f"{self.course}-{self.student}"
+        return f"{self.course}-{self.student}"
+
 
 # Course Rating and Reviews
 class CourseRating(models.Model):
@@ -116,6 +142,7 @@ class CourseRating(models.Model):
     def __str__(self):
         return f"{self.course}-{self.student}-{self.rating}"
 
+
 # Student Favorite Course
 class StudentFavoriteCourse(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
@@ -127,15 +154,18 @@ class StudentFavoriteCourse(models.Model):
 
     def __str__(self):
         return f"{self.course} - {self.student}"
-# Student Assignment
-# class StudentAssignment(models.Model):
-#     student = models.ForeignKey(Student, on_delete=models.CASCADE)
-#     title = models.CharField(max_length=200)
-#     detail = models.TextField(null=True)
-#     add_time = models.DateTimeField(auto_now_add=True)
 
-#     def __str__(self):
-#         return f"{title}"
-    
-#     class Meta:
-#         verbose_name_plural = "9. Student Assignment"
+
+# Student Assignment
+class StudentAssignment(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    detail = models.TextField(null=True)
+    add_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{title}"
+
+    class Meta:
+        verbose_name_plural = "9. Student Assignment"
--- main/serializers.py
+++ main/serializers.py
@@ -1,82 +1,146 @@
 from rest_framework import serializers
 from . import models
 
+
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Teacher
-        fields=["id","full_name", "email", "password", "qualification", "mobile_no","profile_img", "teacher_courses"]
+        fields = [
+            "id",
+            "full_name",
+            "email",
+            "password",
+            "qualification",
+            "mobile_no",
+            "profile_img",
+            "teacher_courses",
+        ]
+
     def __init__(self, *args, **kwargs):
         super(TeacherSerializer, self).__init__(*args, **kwargs)
-        request = self.context.get('request')
+        request = self.context.get("request")
         self.Meta.depth = 0
-        if request and request.method == 'GET':
+        if request and request.method == "GET":
             self.Meta.depth = 1
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = models.CourseCategory
-        fields=["id", "title", "description"]
+        fields = ["id", "title", "description"]
+
+
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Course
-        fields=["id", "category", "teacher", "title", "description", "featured_img", "languages", "course_chapters", "related_videos", "total_enrolled_students", "course_rating"]
+        fields = [
+            "id",
+            "category",
+            "teacher",
+            "title",
+            "description",
+            "featured_img",
+            "languages",
+            "course_chapters",
+            "related_videos",
+            "total_enrolled_students",
+            "course_rating",
+        ]
         depth = 1
+
     def __init__(self, *args, **kwargs):
         super(CourseSerializer, self).__init__(*args, **kwargs)
-        request = self.context.get('request')
+        request = self.context.get("request")
         self.Meta.depth = 0
-        if request and request.method == 'GET':
+        if request and request.method == "GET":
             self.Meta.depth = 1
+
+
 class TeacherDashboardSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Teacher
-        fields = ['total_teacher_courses', 'total_teacher_students', 'total_teacher_chapters']
+        fields = [
+            "total_teacher_courses",
+            "total_teacher_students",
+            "total_teacher_chapters",
+        ]
 
-        
+
 class ChapterSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Chapter
-        fields = ['id', 'course', 'title', 'description', 'video', 'remarks']
+        fields = ["id", "course", "title", "description", "video", "remarks"]
+
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Student
-        fields=["id","full_name", "email", "password", "username", "interested_categories"]
+        fields = [
+            "id",
+            "full_name",
+            "email",
+            "password",
+            "username",
+            "interested_categories",
+        ]
+
 
 class StudentCourseEnrollSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.StudentCourseEnrollment
-        fields = ['id', 'course', 'student', 'enrolled_time']
+        fields = ["id", "course", "student", "enrolled_time"]
 
     def __init__(self, *args, **kwargs):
         super(StudentCourseEnrollSerializer, self).__init__(*args, **kwargs)
-        request = self.context.get('request')
+        request = self.context.get("request")
         self.Meta.depth = 0
-        if request and request.method == 'GET':
+        if request and request.method == "GET":
             self.Meta.depth = 2
 
 
 class CourseRatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.CourseRating
-        fields = ['id', 'course', 'student', 'rating', 'reviews', 'review_time']
+        fields = ["id", "course", "student", "rating", "reviews", "review_time"]
         depth = 1
-        
+
     def __init__(self, *args, **kwargs):
         super(CourseRatingSerializer, self).__init__(*args, **kwargs)
-        request = self.context.get('request')
+        request = self.context.get("request")
         self.Meta.depth = 0
-        if request and request.method == 'GET':
+        if request and request.method == "GET":
             self.Meta.depth = 1
+
 
 class StudentFavoriteCourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.StudentFavoriteCourse
-        fields = ['id', 'course', 'student', 'status']
+        fields = ["id", "course", "student", "status"]
 
     def __init__(self, *args, **kwargs):
         super(StudentFavoriteCourseSerializer, self).__init__(*args, **kwargs)
-        request = self.context.get('request')
+        request = self.context.get("request")
         self.Meta.depth = 0
-        if request and request.method == 'GET':
+        if request and request.method == "GET":
             self.Meta.depth = 2
+
+
+class StudentAssignmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Course
+        fields = [
+            "id",
+            "teacher"
+            "student",
+            "detail",
+            "title",
+            "add_time",
+        ]
+        depth = 1
+
+    def __init__(self, *args, **kwargs):
+        super(CourseSerializer, self).__init__(*args, **kwargs)
+        request = self.context.get("request")
+        self.Meta.depth = 0
+        if request and request.method == "GET":
+            self.Meta.depth = 1
